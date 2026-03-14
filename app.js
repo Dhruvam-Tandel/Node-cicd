6d1a0804d7c4e7887a75f3d603635cb06b51b949
Dhruvam-Tandel <dhruvamtandel12@gmail.com>
2026-03-14 12:32:29 +0530

    this is for testing node-cicd

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("CI/CD Pipeline with GitHub Actions and Docker!");
+});
+
+app.get("/health", (req,res)=>{
+  res.json({status:"OK"});
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
