8a576064cd11aa6c01ac9f3c694d261330a6a4bc
Dhruvam-Tandel <dhruvamtandel12@gmail.com>
2026-03-14 13:16:08 +0530

    solve the error

--- app.js
+++ app.js
@@ -9,10 +9,4 @@ app.get("/health", (req,res)=>{
   res.json({status:"OK"});
 })
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 module.exports = app;
